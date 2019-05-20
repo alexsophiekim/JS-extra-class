4e1a1d9e7fb8da79a04181c0655922d2edf3029e
Sophie Kim <270010565@bun2-211-03.local>
2019-05-20 14:11:58 +1200

    extra Beulas class

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var gallery = document.getElementById('imgs');
+
+var clothes = [
+    {
+      name: "Zara Simple Blouse",
+      price: 90,
+      img:"img/cloth0.jpg"
+    },
+    {
+      name: "Brown Dotted Shirt",
+      price: 65,
+      img:"img/cloth1.jpg"
+    },
+    {
+      name: "White Dress",
+      price: 165,
+      img:"img/cloth2.jpg"
+    },
+    {
+      name: "White Jacket",
+      price: 105,
+      img:"img/cloth3.jpg"
+    },
+    {
+      name: "Kimono Jacket",
+      price: 70,
+      img:"img/cloth4.jpg"
+    },
+    {
+      name: "Midi Dress",
+      price: 120,
+      img:"img/cloth5.jpg"
+    },
+    {
+      name: "Nan Sleeve",
+      price: 120,
+      img:"img/cloth6.jpg"
+    },
+    {
+      name: "Red Jacket",
+      price: 120,
+      img:"img/cloth7.jpg"
+    },
+    {
+      name: "White Button Shirt",
+      price: 60,
+      img:"img/cloth8.jpg"
+    }
+];
+
+var i;
+for (var i = 0; i < clothes.length; i++) {
+  gallery.innerHTML += '<div class ="image-div"> <img class="image" src="img/cloth' +i+ '.jpg"> </div>'
+
+
+}
